921da6fbb2c8e95278e57084168550ef8989fed6
rrghiker <chris.ryan.hart1@gmail.com>
2023-06-23 15:08:02 -0400

    completed first iteration of express & TS integration

---
+++ server/build/routes/loginRoutes.js
@@ -0,0 +1,66 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not permitted');
+}
+const router = (0, express_1.Router)();
+exports.router = router;
+router.get('/login', (req, res) => {
+    res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email"/>
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    // typeguard
+    if (email && password && email === 'hello@hi.com' && password === 'pw') {
+        // mark person as logged in
+        req.session = { loggedIn: true };
+        res.redirect('/');
+    }
+    else {
+        res.send('Invalid email or pw');
+    }
+});
+router.get('/', (req, res) => {
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+    }
+    else {
+        res.send(`
+    <div>
+      <div>You are NOT logged in</div>
+      <a href="/login">Login</a>
+    </div>
+  `);
+    }
+});
+router.get('/logout', (req, res) => {
+    req.session = undefined;
+    res.redirect('/');
+});
+router.get('/protected', requireAuth, (req, res) => {
+    res.send('Welcome to protected route');
+});
